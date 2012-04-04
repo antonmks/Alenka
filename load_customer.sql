1b521348c0bee7d10356b85802c4cb026d3a6bbd
Anton K <antonmks@gmail.com>
2012-04-04 18:57:06 +0400

    added load scripts

---
+++ load_customer.sql
@@ -0,0 +1,2 @@
+C := STREAM 'customer.tbl' USING ('|') AS (c_custkey{1}:int, c_nationkey{4}:int, mktsegment{7}:varchar(10));
+STORE C INTO 'customer' BINARY;
---
+++ load_orders.sql
@@ -0,0 +1,2 @@
+A := STREAM 'orders.tbl' USING ('|') AS (orderkey{1}:int, o_custkey{2}:int, orderdate{5}:int, orderpriority{6}:varchar(15), shippriority{8}:int);
+STORE A INTO 'orders' BINARY;
