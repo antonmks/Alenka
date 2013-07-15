becf0f0f6d6823b00a7626bd19d2951112c382d3
Anton K <antonmks@gmail.com>
2013-07-15 12:57:35 +0300

    Unix compatibility issue fix

--- merge.cu
+++ merge.cu
@@ -57,13 +57,13 @@ struct MurmurHash64D
 
 		switch(*len & 7)
 		{
-			case 7: h ^= unsigned long long(data2[6]) << 48;
-			case 6: h ^= unsigned long long(data2[5]) << 40;
-			case 5: h ^= unsigned long long(data2[4]) << 32;
-			case 4: h ^= unsigned long long(data2[3]) << 24;
-			case 3: h ^= unsigned long long(data2[2]) << 16;
-			case 2: h ^= unsigned long long(data2[1]) << 8;
-			case 1: h ^= unsigned long long(data2[0]);
+			case 7: h ^= uint64_t(data2[6]) << 48;
+			case 6: h ^= uint64_t(data2[5]) << 40;
+			case 5: h ^= uint64_t(data2[4]) << 32;
+			case 4: h ^= uint64_t(data2[3]) << 24;
+			case 3: h ^= uint64_t(data2[2]) << 16;
+			case 2: h ^= uint64_t(data2[1]) << 8;
+			case 1: h ^= uint64_t(data2[0]);
 			h *= m;
 		};
  
@@ -116,13 +116,13 @@ struct MurmurHash64D_F
 
 		switch(*len & 7)
 		{
-			case 7: h ^= unsigned long long(data2[6]) << 48;
-			case 6: h ^= unsigned long long(data2[5]) << 40;
-			case 5: h ^= unsigned long long(data2[4]) << 32;
-			case 4: h ^= unsigned long long(data2[3]) << 24;
-			case 3: h ^= unsigned long long(data2[2]) << 16;
-			case 2: h ^= unsigned long long(data2[1]) << 8;
-			case 1: h ^= unsigned long long(data2[0]);
+			case 7: h ^= uint64_t(data2[6]) << 48;
+			case 6: h ^= uint64_t(data2[5]) << 40;
+			case 5: h ^= uint64_t(data2[4]) << 32;
+			case 4: h ^= uint64_t(data2[3]) << 24;
+			case 3: h ^= uint64_t(data2[2]) << 16;
+			case 2: h ^= uint64_t(data2[1]) << 8;
+			case 1: h ^= uint64_t(data2[0]);
 			h *= m;
 		};
  
