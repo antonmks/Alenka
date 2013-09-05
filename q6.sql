e3ba8799c1219b6a853e603bd5c7bc974df1f922
Anton K <antonmks@gmail.com>
2013-09-05 13:03:37 +0300

    .

--- q6.sql
+++
@@ -1,4 +0,0 @@
-A  :=  LOAD 'lineitem' BINARY AS (l_qty{5}:int, l_price{6}:decimal, l_discount{7}:decimal, l_shipdate{11}:int);
-B := FILTER A BY l_discount >= 0.05 AND l_discount <= 0.07 AND l_qty < 24 AND l_shipdate >= 19940101 AND l_shipdate < 19950101;
-C := SELECT SUM(l_price*l_discount) AS revenue FROM B;			
-STORE C INTO 'mytest.txt' USING ('|') LIMIT 1000;
