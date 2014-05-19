bd0bfa488fcff6086b5495d5f073f33bf21a819b
randolph42 <randolph@deepcloud.com.au>
2014-05-19 17:41:15 +1000

    Added code to show alenkaExecute() call method and a section for calculating QPS

--- main.cu
+++ main.cu
@@ -2,22 +2,39 @@
  	Example main routine for Alenka
 */
 
-#include "alenka.h"
+#include <map>
+#include <string>
 #include <iostream>
-using std::cout;
-using std::endl;
+#include <ctime>
+
+
+using namespace std;
+#include "alenka.h"
 
 int main(int ac, char **av)
 {
+    std::clock_t start;
+    int x;
 
-    if (ac < 2) {
-        cout << "Usage : alenka [-l process_count] [-v] script.sql" << endl;
-        exit(1);
+
+    // test QPS via alenkaExecute	-- this section is the only C++ dependency
+    if (string(av[1]) == "--QPS-test") {
+        alenkaInit(NULL);
+        start = std::clock();
+        for (x=0; x< 1000; x++)  {
+       	    alenkaExecute("A1 := SELECT  count(n_name) AS col1 FROM nation;\n DISPLAY A1 USING ('|');");
+        }
+        cout<< "Ave QPS is : " <<  ( 1000/ (( std::clock() - start ) / (double)CLOCKS_PER_SEC )) << endl;
+        alenkaClose();
     }
-	else {
-
-		return execute_file( ac, av) ;
-	};	
+    else {				// ordinary alenka file mode
+        if (ac < 2) {
+            cout << "Usage : alenka [--QPS-test] | [ [-l process_count] [-v] script.sql ]" << endl;
+            exit(1);
+        }
+        else
+	    return execute_file( ac, av) ;
+    }
 }
 
 
