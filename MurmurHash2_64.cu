c97eda6a9b0b30cac042d8ed5dd24cdcf989b920
Anton K <antonmks@gmail.com>
2014-06-26 06:24:34 +0300

    .

--- MurmurHash2_64.cu
+++ MurmurHash2_64.cu
@@ -6,11 +6,59 @@
 
 #include <stdint.h>
 
+
 #ifdef _WIN64
 typedef unsigned __int64 uint64_t;
 #endif
 
 // 64-bit hash for 64-bit platforms
+
+uint64_t MurmurHash64S ( const void * key, int len, unsigned int seed, unsigned int step, size_t count )
+{
+	const uint64_t m = 0xc6a4a7935bd1e995;
+	const int r = 47;
+
+	uint64_t h = seed ^ (len * step * m);
+
+	const uint64_t * data = (const uint64_t *)key;
+	const uint64_t * end = data + count*step;
+
+	while(data != end)
+	{
+		uint64_t k = *data;
+		
+
+		k *= m; 
+		k ^= k >> r; 
+		k *= m; 
+		
+		h ^= k;
+		h *= m; 
+		data = data+count;
+	}
+
+	const unsigned char * data2 = (const unsigned char*)data;
+
+	switch(len & 7)
+	{
+	case 7: h ^= uint64_t(data2[6]) << 48;
+	case 6: h ^= uint64_t(data2[5]) << 40;
+	case 5: h ^= uint64_t(data2[4]) << 32;
+	case 4: h ^= uint64_t(data2[3]) << 24;
+	case 3: h ^= uint64_t(data2[2]) << 16;
+	case 2: h ^= uint64_t(data2[1]) << 8;
+	case 1: h ^= uint64_t(data2[0]);
+	        h *= m;
+	};
+ 
+	h ^= h >> r;
+	h *= m;
+	h ^= h >> r;
+
+	return h;
+} 
+
+
 
 uint64_t MurmurHash64A ( const void * key, int len, unsigned int seed )
 {
