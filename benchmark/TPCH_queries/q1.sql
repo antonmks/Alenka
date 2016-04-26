35786fb6c776a6b50412edae591826b98f1e07d8
Alexander Hurd <hurdad@gmail.com>
2016-04-26 02:56:57 -0400

    update

--- benchmark/TPCH_queries/q1.sql
+++ benchmark/TPCH_queries/q1.sql
@@ -1,7 +1,16 @@
 B := FILTER lineitem BY shipdate <= 19980902;
-D := SELECT returnflag AS rf1, linestatus AS lf1, SUM(price) AS price_sum, SUM(qty) AS sum_qty,AVG(qty) AS avg_qty,
-			AVG(price) AS avg_price, AVG(discount) AS avg_disc, SUM((1+tax)*(1-discount)*price) AS sum_d,
-			SUM((1-discount)*price) AS discount_price, AVG(tax) AS tax_avg,
-			COUNT(qty) AS qq
-	 FROM B	GROUP BY returnflag, linestatus;
-STORE D INTO 'q1.txt' USING ('|');
+D := SELECT returnflag AS rf1, 
+linestatus AS lf1,
+SUM(qty) AS sum_qty,
+SUM(price) AS price_sum, 
+SUM((1-discount)*price) AS discount_price,
+SUM((1+tax)*(1-discount)*price) AS sum_d,
+AVG(qty) AS avg_qty,
+AVG(price) AS avg_price,
+AVG(discount) AS avg_disc,
+COUNT(qty) AS count_order
+FROM B	GROUP BY returnflag, linestatus;
+
+DO := ORDER D BY rf1, lf1;
+
+STORE DO INTO 'q1.txt' USING ('|');
